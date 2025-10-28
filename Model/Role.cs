306bb1b995c4a18f65de16f53c9fd3c3dc21ee81
Khasanova <nurgiza.hasanova@yandex.ru>
2025-10-28 05:54:16 +0500

    классы в папке view

--- Model/Role.cs
+++ Model/Role.cs
@@ -6,7 +6,18 @@ using System.Threading.Tasks;
 
 namespace Lab_rab_4._1_KhasanovaNG_BPI_23_01.Model
 {
-    internal class Role
+    public class Role
     {
+        public int Id { get; set; }
+        public string NameRole { get; set; }
+
+        public Role() { }
+
+        public Role(int id, string nameRole)
+        {
+            this.Id = id;
+            this.NameRole = nameRole;
+        }
     }
+
 }
