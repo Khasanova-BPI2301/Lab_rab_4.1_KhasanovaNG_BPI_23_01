50882a343c2b5091a153ef579b6f4fe82f3d4cef
Khasanova <nurgiza.hasanova@yandex.ru>
2025-10-28 05:38:52 +0500

    классы в папке viewmodel

--- obj/Debug/net8.0-windows/Lab_rab_4.1_KhasanovaNG_BPI_23_01.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/Lab_rab_4.1_KhasanovaNG_BPI_23_01.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab_rab_4.1_KhasanovaNG_BPI_23_01")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ccb89eb8012dd037996d55bf8f400c56e60e0a4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1b84b4fa9d603fdf052cadd320998c8c3ae251f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab_rab_4.1_KhasanovaNG_BPI_23_01")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab_rab_4.1_KhasanovaNG_BPI_23_01")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
