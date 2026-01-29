8b7e2b5fee2ac1580fdba084901cdc1b4998bb7b
Khasanova <nurgiza.hasanova@yandex.ru>
2026-01-29 21:27:23 +0500

    добвила редактирование удаление и добавление плюс создала вспомогательный класс

--- View/WindowEmployee.xaml.cs
+++ View/WindowEmployee.xaml.cs
@@ -23,11 +23,15 @@ namespace Lab_rab_4._1_KhasanovaNG_BPI_23_01.View
     /// </summary>
     public partial class WindowEmployee : Window
     {
+        private ObservableCollection<PersonDPO> persons;
+        private PersonViewModel vmPerson;
+        private RoleViewModel vmRole;
+
         public WindowEmployee()
         {
             InitializeComponent();
-            PersonViewModel vmPerson = new PersonViewModel();
-            RoleViewModel vmRole = new RoleViewModel();
+            vmPerson = new PersonViewModel();
+            vmRole = new RoleViewModel();
 
             List<Role> roles = new List<Role>();
             foreach (Role r in vmRole.ListRole)
@@ -35,7 +39,7 @@ namespace Lab_rab_4._1_KhasanovaNG_BPI_23_01.View
                 roles.Add(r);
             }
 
-            ObservableCollection<PersonDPO> persons = new ObservableCollection<PersonDPO>();
+            persons = new ObservableCollection<PersonDPO>();
             FindRole finder;
 
             foreach (var p in vmPerson.ListPerson)
@@ -54,7 +58,93 @@ namespace Lab_rab_4._1_KhasanovaNG_BPI_23_01.View
 
             lvEmployee.ItemsSource = persons;
         }
+
+        private void BtnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new WindowInputDialog("Добавление сотрудника");
+            if (dialog.ShowDialog() == true)
+            {
+                int newId = persons.Count > 0 ? persons[persons.Count - 1].Id + 1 : 1;
+                persons.Add(new PersonDPO
+                {
+                    Id = newId,
+                    LastName = dialog.LastName,
+                    FirstName = dialog.FirstName,
+                    Role = dialog.SelectedRole,
+                    Birthday = dialog.Birthday
+                });
+
+                // Также добавляем в исходный список (если нужно)
+                vmPerson.ListPerson.Add(new Person
+                {
+                    Id = newId,
+                    LastName = dialog.LastName,
+                    FirstName = dialog.FirstName,
+                    RoleId = GetRoleIdByName(dialog.SelectedRole),
+                    Birthday = dialog.Birthday
+                });
+            }
+        }
+
+        private void BtnEdit_Click(object sender, RoutedEventArgs e)
+        {
+            var selected = lvEmployee.SelectedItem as PersonDPO;
+            if (selected == null)
+            {
+                MessageBox.Show("Выберите сотрудника для редактирования.");
+                return;
+            }
+
+            var dialog = new WindowInputDialog("Редактирование", selected);
+            if (dialog.ShowDialog() == true)
+            {
+                selected.LastName = dialog.LastName;
+                selected.FirstName = dialog.FirstName;
+                selected.Role = dialog.SelectedRole;
+                selected.Birthday = dialog.Birthday;
+
+                // Обновляем в исходном списке
+                var person = new List<Person>(vmPerson.ListPerson).Find(p => p.Id == selected.Id);
+                if (person != null)
+                {
+                    person.LastName = selected.LastName;
+                    person.FirstName = selected.FirstName;
+                    person.RoleId = GetRoleIdByName(selected.Role);
+                    person.Birthday = selected.Birthday;
+                }
+            }
+        }
+
+        private void BtnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            var selected = lvEmployee.SelectedItem as PersonDPO;
+            if (selected == null)
+            {
+                MessageBox.Show("Выберите сотрудника для удаления.");
+                return;
+            }
+
+            if (MessageBox.Show($"Удалить {selected.LastName} {selected.FirstName}?", "Подтверждение", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
+            {
+                persons.Remove(selected);
+                var personToRemove = new List<Person>(vmPerson.ListPerson).Find(p => p.Id == selected.Id);
+                if (personToRemove != null)
+                {
+                    vmPerson.ListPerson.Remove(personToRemove);
+                }
+            }
+        }
+
+        private int GetRoleIdByName(string roleName)
+        {
+            foreach (var r in vmRole.ListRole)
+                if (r.NameRole == roleName)
+                    return r.Id;
+            return 0;
+        }
     }
 }
+    
+
     
 
---
+++ WindowInputDialog.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Lab_rab_4._1_KhasanovaNG_BPI_23_01.Model;
+using Lab_rab_4._1_KhasanovaNG_BPI_23_01.ViewModel;
+
+namespace Lab_rab_4._1_KhasanovaNG_BPI_23_01
+{
+    /// <summary>
+    /// Логика взаимодействия для WindowInputDialog.xaml
+    /// </summary>
+    public partial class WindowInputDialog : Window
+    {
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string SelectedRole { get; set; }
+        public DateTime Birthday { get; set; }
+        public List<string> Roles { get; set; }
+        public WindowInputDialog(string title, PersonDPO existing = null)
+        {
+            InitializeComponent();
+
+            Title = title;
+            var vmRole = new RoleViewModel();
+            Roles = new List<string>();
+            foreach (var r in vmRole.ListRole)
+                Roles.Add(r.NameRole);
+
+            if (existing != null)
+            {
+                LastName = existing.LastName;
+                FirstName = existing.FirstName;
+                SelectedRole = existing.Role;
+                Birthday = existing.Birthday;
+            }
+            else
+            {
+                Birthday = DateTime.Today;
+                SelectedRole = Roles.Count > 0 ? Roles[0] : "";
+            }
+
+            DataContext = this;
+        }
+
+        private void Ok_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+            Close();
+        }
+
+        private void Cancel_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+            Close();
+        }
+    }
+}
